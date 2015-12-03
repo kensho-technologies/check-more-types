8e9dfdf48e99821764854dc80e3733c252d6d601
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-12-03 11:02:10 -0500

    test(ci): trying to work around travis jobs problems in semantic release

---
+++ test/publish-from-specific-node.js
@@ -0,0 +1,35 @@
+function validTravis(pluginConfig, config) {
+  var env = config.env;
+  var options = config.options;
+  return env.TRAVIS === 'true' &&
+    !env.TRAVIS_TAG &&
+    options.branch === env.TRAVIS_BRANCH;
+}
+
+function publishFromSpecificNode(options, config, callback) {
+  console.log('plugin options', options);
+
+  function fail(message) {
+    return callback(new Error(message));
+  }
+
+  if (typeof options.node !== 'string') {
+    return fail('Missing node version in the config');
+  }
+
+  if (process.versions.node !== options.node) {
+    return fail('Only allowed to publish from Node ' +
+      options.node + ' not from ' + process.versions.node);
+  }
+
+  // check Travis environment for now
+  // maybe it will be not necessary in the future
+  // https://github.com/semantic-release/semantic-release/issues/141
+  if (!validTravis(options, config)) {
+    return fail('Travis is invalid');
+  }
+
+  callback(new Error('nope'), null);
+}
+
+module.exports = publishFromSpecificNode;
