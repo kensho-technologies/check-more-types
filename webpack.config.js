30bf06750e7eb5d644736c376e8d8359f8f9f774
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-04-05 16:19:31 -0400

    started webpack conversion

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  output: {
+    library: 'checkMoreTypes',
+    libraryTarget: 'umd',
+    path: './dist',
+    filename: 'check-more-types.js'
+  },
+  entry: {
+    library: './src/index.js'
+  }
+}
